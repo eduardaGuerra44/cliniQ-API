f53bdf7237cb1637b1fe3169c572e284d1798421
Eduarda Guerra <eduardaguerra44@gmail.com>
2025-01-07 21:07:06 -0300

    adição de campo no DB e criação do método para listar médicos

--- src/main/java/API/cliniQ/controller/MedicoController.java
+++ src/main/java/API/cliniQ/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package API.cliniQ.controller;
 
 import API.cliniQ.medicos.DadosCadastroMedico;
+import API.cliniQ.medicos.DadosListagemMedico;
 import API.cliniQ.medicos.Medico;
 import API.cliniQ.medicos.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,5 +23,10 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
 
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listarMedico(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
